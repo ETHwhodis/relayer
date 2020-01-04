cc2be2dd6e74949d7dd9b6f58caae0694d85a22b
whodis <balajsri@balajsris-MacBook-Air.local>
2020-01-03 20:38:23 -0500

    fix port

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ module.exports = {
   },
   defaultGasPrice: 2,
   gasOracleUrls: ['https://ethgasstation.info/json/ethgasAPI.json', 'https://gasprice.poa.network/'],
-  port: process.env.APP_PORT,
+  port: process.env.PORT || process.env.APP_PORT,
   relayerServiceFee: Number(process.env.RELAYER_FEE),
   deploymentTxs: {
     // TORNADO ETH MAINNET
